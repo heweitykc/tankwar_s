f6915c9d9d883f66ffe207b720d6467ea9df41fd
callee <240100889@qq.com>
2016-10-08 10:41:57 +0800

    2

--- srv/src/main.go
+++ srv/src/main.go
@@ -1,10 +1,43 @@
 ﻿package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"net/http"	
 	"github.com/gorilla/websocket"
 )
 
+var addr = flag.String("addr", "0.0.0.0:8080", "http service address")
+var upgrader = websocket.Upgrader{}
+
+func socketHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
 func main() {
-	fmt.Println("。。。。。。")
+	fmt.Println(".....")	
+	http.HandleFunc("/game", socketHandler)
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
