ea2434d6faa9332a45f4c78f25bc2adedc7cc28f
callee <240100889@qq.com>
2016-10-17 09:27:26 +0800

    test1

---
+++ srv/src/test/test1.go
@@ -0,0 +1,23 @@
+﻿package main
+
+import (
+	"fmt"
+	"time"
+//	"flag"	
+)
+
+func main() {	
+	var ticker = time.NewTicker(time.Second)
+	var cnt = 0
+	for{
+		select{
+			case <-ticker.C:
+				fmt.Printf("loop: cnt=%d\n", cnt)
+			default:
+				cnt++
+				if cnt%1e6 == 0 {
+					fmt.Printf("M")
+				}
+		}
+	}
+}
